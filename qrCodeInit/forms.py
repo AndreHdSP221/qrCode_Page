525290d57690029a0b8f6a0d55a1faeff6ce0a38
AndreHdSP221 <andrehsp22@gmail.com>
2025-06-22 12:41:31 -0300

    Projeto reforumado. Retidada dos adesivos arbo. Preparando estrutura para receber sistema de login/logout e A2F.

--- qrCodeInit/forms.py
+++ qrCodeInit/forms.py
@@ -15,24 +15,3 @@ class GeradorQrCodeZipForm(forms.Form):
             attrs={'class': 'form-control', 'placeholder': 'Ex: 50'}
         ),
     )
-
-class GerarAdesivoArboForm(forms.Form):
-    codigo_inicial = forms.CharField(
-        label='Código Inicial',
-        max_length=100,
-        widget=forms.TextInput(
-            attrs={'class': 'form-control', 'placeholder': 'Ex: ARBO-001'}
-        ),
-    )
-    quantidade = forms.IntegerField(
-        label='Quantidade de Adesivos',
-        min_value=1,
-        widget=forms.NumberInput(
-            attrs={'class': 'form-control', 'placeholder': 'Ex: 10'}
-        ),
-    )
-    cidade_logo = forms.ImageField(
-        label='Logo da Cidade (enviada pelo usuário)',
-        allow_empty_file=False,
-        widget=forms.FileInput(attrs={'class': 'form-control-file'}),
-    )
--- qrCodeInit/urls.py
+++ qrCodeInit/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gerar_zip_qrcodes, name='gerar_zip_qrcodes'), # Substituir home por um menu
-    path('arbomonitor/', views.gerar_adesivos_arbo, name='AdesivosArbo'),
+    path('', views.gerar_zip_qrcodes, name='gerar_zip_qrcodes'), # Substituir home por um menu com varias opções
 ]
